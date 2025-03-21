4e9557b9333a796979a5382c888c097893675813
Akshit Yadav <akshit07032001@gmail.com>
2025-03-21 22:09:01 +0530

    setup middleware

---
+++ backend/routes/middleware.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt');
+const { JWT_SECRET } = require('../config');
+
+const authMiddleWare = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(403).json({});
+    }
+    const token = authHeader.split(' ')[1];
+
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET);
+
+      req.userId = decoded.userId;
+
+      next();
+    } catch (err) {
+      return res.status(403).json({});
+    }
+  } catch (error) {
+    console.error('Error doing the middleware check!', error);
+    res.status(500).json({ error: 'Error doing the middleware check!' });
+  }
+};
+
+module.exports = { authMiddleWare };
